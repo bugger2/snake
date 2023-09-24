f15727aadb2216a4b8c4ff0d003ade088f7e0204
Cedar <usernamesarenoteasy@proton.me>
2023-09-24 01:08:25 -0600

    cleaned up the code a bit (still doesn't work)

--- src/dynarray.h
+++ src/dynarray.h
@@ -4,15 +4,25 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+// A standard dynamic array
 typedef struct Dynarray {
 	size_t size;
 	int* ptr;
-	void (*append)(struct Dynarray* ptr, int value);
-	void (*prepend)(struct Dynarray* ptr, int value);
-	void (*deleteEnd)(struct Dynarray* ptr);
-	void (*deleteStart)(struct Dynarray* ptr);
 } Dynarray;
 
-Dynarray dynarray_create(size_t size, ...); // pretty please do not initialize a dynamic array with more arguments than size
+// Create a new dynamic array with size amount of elements
+Dynarray dynarray_create(size_t size);
+
+// Append value to the end of the dynamic array dynarray
+void dynarray_append(Dynarray* dynarray, int value);
+
+// Prepend value to the end of the dynamic array dynarray
+void dynarray_prepend(Dynarray* dynarray, int value);
+
+// Delete the item at the end of a dynamic array
+void dynarray_deleteEnd(Dynarray* dynarray);
+
+// Delete the item at the start of a dynamic array
+void dynarray_deleteStart(Dynarray* dynarray);
 
 #endif // DYNARRAY_H_
